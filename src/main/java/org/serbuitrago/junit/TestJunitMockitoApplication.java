ac09b82fbfa9276d8dfedb8a7f353326f1dade75
Sergio Buitrago <sergiostivesbb@ufps.edu.co>
2022-01-21 15:40:12 -0500

    Init

---
+++ src/main/java/org/serbuitrago/junit/TestJunitMockitoApplication.java
@@ -0,0 +1,13 @@
+package org.serbuitrago.junit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJunitMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJunitMockitoApplication.class, args);
+	}
+
+}
