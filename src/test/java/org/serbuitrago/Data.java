0feb977e292bfa8b6e9197890aa220ba2d52fcac
Sergio Buitrago <sergiostivesbb@ufps.edu.co>
2022-01-24 17:05:58 -0500

    Exceptions

--- src/test/java/org/serbuitrago/Data.java
+++ src/test/java/org/serbuitrago/Data.java
@@ -11,10 +11,15 @@ public class Data {
 			new Quiz(7L, "Estadistica"), new Quiz(8L, "Filosofia"), new Quiz(9L, "Tecnologia"),
 			new Quiz(10L, "Lengua Castellena"), new Quiz(11L, "Frances"), new Quiz(12L, "Italiano"));
 
+	public static final List<Quiz> DATA_LIST_QUIZ_ID_NULL = Arrays.asList(new Quiz(null, "Matematica"),
+			new Quiz(null, "Religion"), new Quiz(null, "Etica"), new Quiz(null, "Social"), new Quiz(null, "Ingles"),
+			new Quiz(null, "Informatica"), new Quiz(null, "Estadistica"), new Quiz(null, "Filosofia"),
+			new Quiz(null, "Tecnologia"), new Quiz(null, "Lengua Castellena"), new Quiz(null, "Frances"),
+			new Quiz(null, "Italiano"));
+
 	public static final List<String> DATA_LIST_QUESTION = Arrays.asList("Suma", "Resta", "Derivadas", "Integrales",
 			"Ecuaciones diferenciales");
-	
-	
-	public static final Quiz DATA_QUIZ = new Quiz(13L, "Calculo Vectorial", DATA_LIST_QUESTION);
-	
+
+	public static final Quiz DATA_QUIZ = new Quiz(null, "Calculo Vectorial", DATA_LIST_QUESTION);
+
 }
