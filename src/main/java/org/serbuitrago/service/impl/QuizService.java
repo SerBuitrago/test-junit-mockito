2bf01b878fbd86a7043726b3d40a175915479e87
Sergio Buitrago <sergiostivesbb@ufps.edu.co>
2022-01-24 10:35:37 -0500

    Implements

---
+++ src/main/java/org/serbuitrago/service/impl/QuizService.java
@@ -0,0 +1,19 @@
+package org.serbuitrago.service.impl;
+
+import org.serbuitrago.model.Quiz;
+import org.serbuitrago.repository.IQuizRepository;
+import org.serbuitrago.service.IQuizService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class QuizService implements IQuizService{
+	
+	private IQuizRepository iQuizRepository;
+	
+	@Override
+	public Quiz findByName(String name) {
+		return iQuizRepository.findAll().stream().filter(quiz -> quiz.getName().contains(name)).findFirst()
+				.orElseThrow();
+	}
+}
